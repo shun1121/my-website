979fa8da664580240ea4819dc88352dc0a03925c
shun1121 <hikitanikun.ware80000@gmail.com>
2022-08-19 10:52:48 +0900

    レイアウトコンポーネントを作成

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+// import { HeaderSimple } from '../components/header'
+import { links } from '../components/link'
+import Layout from '../components/layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,9 +1,6 @@
 import React, { FC } from 'react';
 import { blogList } from "../components/blogList";
 import BlogListSection from "../components/blogListSection";
-import { HeaderSimple } from '../components/header';
-import { links } from '../components/link';
-import { Footer } from '../components/footer';
 
 type BlogList = {
   blogList: {    
@@ -16,9 +13,7 @@ type BlogList = {
 const Blog: FC<BlogList> = () => {
   return (
     <div>
-      <HeaderSimple links={links} />
       <BlogListSection blogList={blogList} />
-      <Footer />
     </div>
   )
 }
