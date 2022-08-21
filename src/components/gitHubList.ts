834aa62f3507c0d5896ba869ea8ff2f1238ebfbe
shun1121 <hikitanikun.ware80000@gmail.com>
2022-08-22 01:52:29 +0900

    githubコンポーネント追加

---
+++ src/components/gitHubList.ts
@@ -0,0 +1,22 @@
+export const githubList = [
+  {
+    repositoryName: "lightsound/nexst-tailwind",
+    description: "Next.js starter template."
+  },
+  {
+    repositoryName: "lightsound/nexst-tailwind",
+    description: "Next.js starter template."
+  },
+  {
+    repositoryName: "lightsound/nexst-tailwind",
+    description: "Next.js starter template."
+  },
+  {
+    repositoryName: "lightsound/nexst-tailwind",
+    description: "Next.js starter template."
+  },
+  {
+    repositoryName: "lightsound/nexst-tailwind",
+    description: "Next.js starter template."
+  },
+]
