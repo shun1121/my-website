97667c94b7b136c71509eab2cc270c21597d62af
shun1121 <hikitanikun.ware80000@gmail.com>
2022-09-05 17:51:06 +0900

    api周り修正

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -2,6 +2,6 @@ import { createClient } from 'microcms-js-sdk'; //ES6
 
 // Initialize Client SDK.
 export const client = createClient({
-  serviceDomain: process.env.NEXT_PUBLIC_SERVICE_DOMAIN || '', // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
-  apiKey: process.env.NEXT_PUBLIC_APIKEY || '',
+  serviceDomain: process.env.SERVICE_DOMAIN || '', // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
+  apiKey: process.env.APIKEY || '',
 });
