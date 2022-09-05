28b62944ba4621122fcd72cd478c9dd235dca8ee
shun1121 <hikitanikun.ware80000@gmail.com>
2022-09-05 17:30:15 +0900

    変数名変更

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -2,6 +2,6 @@ import { createClient } from 'microcms-js-sdk'; //ES6
 
 // Initialize Client SDK.
 export const client = createClient({
-  serviceDomain: 'my-website', // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
-  apiKey: 'f38f42d07e7342f8b0458ddfd21374b1e57c',
+  serviceDomain: process.env.NEXT_PUBLIC_SERVICE_DOMAIN || '', // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
+  apiKey: process.env.NEXT_PUBLIC_APIKEY || '',
 });
