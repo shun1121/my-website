0832c3ee91e5ac170c9b4549af2e5c8b47b3e172
shun1121 <hikitanikun.ware80000@gmail.com>
2022-09-30 01:32:01 +0900

    不要ログ削除、cmsでwebhook作成

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -4,7 +4,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log("aaaaaaaaaaaaaaaaaaaaaaa")
   // Check for secret to confirm this is a valid request
   if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
     return res.status(401).json({ message: 'Invalid token' });
