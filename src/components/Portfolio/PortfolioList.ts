1aad247df511336924fdac55e6a1b04728d02640
shun1121 <hikitanikun.ware80000@gmail.com>
2022-10-13 16:14:03 +0900

    fix: ポートフォリオ修正

--- src/components/Portfolio/PortfolioList.ts
+++ src/components/Portfolio/PortfolioList.ts
@@ -1,38 +1,32 @@
 export const portfolio = [
   {
     imgUrl: "/images/kv.jpg",
-    title: "IT KINGDOM",
-    description: "当サロンのLPページ。React、Next.js、TypeScriptなどのモダンな技術を用いて作られています。初心者にちょうど良い難易度の制作物です。",
+    title: "ねこねこ",
+    description: "test test test test test test test test test test test test test test test test test test test test test test",
     date: "2022.07.11"
   },
   {
     imgUrl: "/images/kv.jpg",
-    title: "IT KINGDOM",
-    description: "当サロンのLPページ。React、Next.js、TypeScriptなどのモダンな技術を用いて作られています。初心者にちょうど良い難易度の制作物です。",
+    title: "ねこねこ",
+    description: "test test test test test test test test test test test test test test test test test test test test test test",
     date: "2022.07.11"
   },
   {
     imgUrl: "/images/kv.jpg",
-    title: "IT KINGDOM",
-    description: "当サロンのLPページ。React、Next.js、TypeScriptなどのモダンな技術を用いて作られています。初心者にちょうど良い難易度の制作物です。",
+    title: "ねこねこ",
+    description: "test test test test test test test test test test test test test test test test test test test test test test",
     date: "2022.07.11"
   },
   {
     imgUrl: "/images/kv.jpg",
-    title: "IT KINGDOM",
-    description: "当サロンのLPページ。React、Next.js、TypeScriptなどのモダンな技術を用いて作られています。初心者にちょうど良い難易度の制作物です。",
+    title: "ねこねこ",
+    description: "test test test test test test test test test test test test test test test test test test test test test test",
     date: "2022.07.11"
   },
   {
     imgUrl: "/images/kv.jpg",
-    title: "IT KINGDOM",
-    description: "当サロンのLPページ。React、Next.js、TypeScriptなどのモダンな技術を用いて作られています。初心者にちょうど良い難易度の制作物です。",
-    date: "2022.07.11"
-  },
-  {
-    imgUrl: "/images/kv.jpg",
-    title: "IT KINGDOM",
-    description: "当サロンのLPページ。React、Next.js、TypeScriptなどのモダンな技術を用いて作られています。初心者にちょうど良い難易度の制作物です。",
+    title: "ねこねこ",
+    description: "test test test test test test test test test test test test test test test test test test test test test test",
     date: "2022.07.11"
   },
 ]
